95143f2bf6353ee61beff3b5b0ea6ded1420e2ba
TahLoow <macleanp98@gmail.com>
2026-01-12 22:15:16 -0500

    replaced tailwind.config with app.css (tailwind v4). Restructured layout

---
+++ src/lib/icons/linkedin.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	const { class: myClass } = $props();
+</script>
+
+<svg
+	xmlns="http://www.w3.org/2000/svg"
+	viewBox="0 0 24 24"
+	fill="white"
+	class={myClass}
+	aria-hidden="true"
+>
+	<path
+		d="M4.98 3.5C4.98 4.88 3.87 6 2.5 6S0 4.88 0 3.5 1.11 1 2.5 1 4.98 2.12 4.98 3.5zM.5 23.5h4V7.5h-4v16zM8.5 7.5h3.8v2.2h.05c.53-1 1.82-2.2 3.75-2.2 4 0 4.75 2.6 4.75 6v9.5h-4v-8.4c0-2-.04-4.6-2.8-4.6-2.8 0-3.2 2.2-3.2 4.4v8.6h-4v-16z"
+	/>
+</svg>
---
+++ src/routes/Footer.svelte
@@ -0,0 +1,49 @@
+<script lang="ts">
+	import { Portal, Tooltip } from '@skeletonlabs/skeleton-svelte';
+</script>
+
+{#snippet bookItem(title: string, author: string, href: string)}
+	<li class="mt-1">
+		<a {href}>
+			<p class="rounded-sm px-1 hover:bg-white">
+				<span class="font-semibold">{title}</span> <span class="italic"> by {author}</span>
+			</p>
+		</a>
+	</li>
+{/snippet}
+
+<footer class="">
+	<div class="flex justify-center">
+		<ul>
+			<p class="mb-4 w-full text-lg font-semibold">Current Reads</p>
+			{@render bookItem('Darkness of Creation', 'Nicholas Carrol', 'https://a.co/d/9xh3Th2')}
+			{@render bookItem(
+				'Catalyst: A Rogue One Novel',
+				'James Luceno',
+				'https://www.hpb.com/search?q=+9781101967003'
+			)}
+			{@render bookItem(
+				'On Writing',
+				'Stephen King',
+				'https://www.hpb.com/search?q=+9780671582364'
+			)}
+		</ul>
+		<div class=""></div>
+	</div>
+
+	<Tooltip positioning={{ placement: 'top' }}>
+		<Tooltip.Trigger>🦶</Tooltip.Trigger>
+		<Portal>
+			<Tooltip.Positioner>
+				<Tooltip.Content class="card preset-filled-surface-950-50 p-2">
+					<span>🥁🥁🐍</span>
+					<Tooltip.Arrow
+						class="[--arrow-background:var(--color-surface-950-50)] [--arrow-size:--spacing(2)]"
+					>
+						<Tooltip.ArrowTip />
+					</Tooltip.Arrow>
+				</Tooltip.Content>
+			</Tooltip.Positioner>
+		</Portal>
+	</Tooltip>
+</footer>
