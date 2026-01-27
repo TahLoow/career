945344b5ce8da4c18e6e25885448e3cc53c8a061
TahLoow <macleanp98@gmail.com>
2026-01-26 19:39:39 -0500

    changed from adapter-auto to adapter-static, added ssr=false export to layout

---
+++ src/routes/background/+page.svelte
@@ -0,0 +1,12 @@
+<svelte:head>
+	<title>Paul - Background</title>
+	<meta name="Background" content="My background" />
+</svelte:head>
+
+<div class="flex w-full justify-center">
+	<div class="card bg-surface-200-800 mt-8 w-2/3 flex-col px-8 py-6">
+		<h1 class="font-accent pb-4 text-5xl font-bold italic">Background</h1>
+
+		<div class="prose mb-4">This page is a work in progress</div>
+	</div>
+</div>
