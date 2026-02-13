6a21220f57a9ac3cc061bb4e18afab4187a67f48
TahLoow <macleanp98@gmail.com>
2026-02-13 13:12:11 -0500

    began post

---
+++ src/routes/posts/pixel-board/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+	import BannerImage from '$lib/features/posts/banner-image/BannerImage.svelte';
+	import banner from './images/banner.png';
+</script>
+
+<BannerImage src={banner} alt="" />
+
+<div class="prose mb-2 self-center md:w-3/5">
+	<h1>Software Project: Pixel Board</h1>
+	<hr class="hr" />
+
+	<h2>What</h2>
+	<p>
+		The "pixel board" is an anonymous, collaborative, realtime drawing experiment. Visitors can
+		place a couple pixels to alter the design of the board.
+	</p>
+</div>
