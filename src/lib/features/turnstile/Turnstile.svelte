f8cf8f179be46c349f4fb218de2f715f294b61e3
TahLoow <macleanp98@gmail.com>
2026-02-18 19:31:20 -0500

    replaced top level await with window callback

--- src/lib/features/turnstile/Turnstile.svelte
+++ src/lib/features/turnstile/Turnstile.svelte
@@ -1,17 +1,3 @@
-<script lang="ts" module>
-	const script = document.createElement('script');
-	script.src = 'https://challenges.cloudflare.com/turnstile/v0/api.js?render=explicit';
-	script.async = true;
-	document.body.appendChild(script);
-
-	await new Promise<unknown>((resolve, reject) => {
-		script.addEventListener('load', resolve, { once: true });
-		script.addEventListener('error', reject, { once: true });
-	});
-
-	const turnstile = window.turnstile;
-</script>
-
 <script lang="ts">
 	import { onMount } from 'svelte';
 	import type { TurnstileState } from './turnstile-state.svelte';
@@ -24,7 +10,10 @@
 
 	onMount(() => {
 		// @ts-ignore
-		turnstileState.loadWidget(turnstile);
+		window.initTurnstile = () => {
+			const turnstile = window.turnstile;
+			turnstileState.loadWidget(turnstile);
+		};
 	});
 </script>
 
